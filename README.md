7093dda1a66f6ce537517ffe705ca8458655cfda
Antonio Henrique <88168605+Henriquedacruz@users.noreply.github.com>
2022-08-15 08:36:31 -0300

    imageSave

---
+++ README.md
@@ -0,0 +1,18 @@
+# API_alunos_professores
+
+# Sobre o multer
+
+https://www.npmjs.com/package/multer
+
+# dependências
+
+--Multer
+--Express
+
+# teste insominia 
+
+http://localhost:8080/upload-image
+
+content-type : multipart/form-data
+
+ 
---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const uploadUser = require('./middlewares/uploadImage');
+
+
+app.post("/upload-image", uploadUser.single('image'), async (req, res) => {
+
+    if (req.file) {
+        console.log(req.file);
+        return res.json({
+            erro: false,
+            mensagem: "Upload realizado com sucesso!"
+        });
+    }
+
+    return res.status(400).json({
+        erro: true,
+        mensagem: "Erro: Upload não realizado com sucesso, necessário enviar uma imagem PNG ou JPG!"
+    });
+
+
+
+});
+
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080");
+});
