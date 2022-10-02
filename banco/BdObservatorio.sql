c28ecf2005010dea66ada15e2911ca3b3a2830f1
Antonio Henrique <88168605+Henriquedacruz@users.noreply.github.com>
2022-10-01 22:46:56 -0300

    bd_upload
    banco de dados sql para testes

---
+++ banco/BdObservatorio.sql
@@ -0,0 +1,9 @@
+#drop database observatorio;
+
+create database observatorio character set utf8mb4 collate utf8mb4_unicode_ci;
+use observatorio;
+
+select * from observatorio.imagens;
+
+
+#delete from imagens where id = 9;
