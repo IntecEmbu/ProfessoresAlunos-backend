7deaecaf05bb728b584583ea17b789177171841d
Antonio Henrique <88168605+Henriquedacruz@users.noreply.github.com>
2022-10-05 14:55:02 -0300

    tblTesteUser

--- banco/BdObservatorio.sql
+++ banco/BdObservatorio.sql
@@ -5,5 +5,13 @@ use observatorio;
 
 select * from observatorio.imagens;
 
+create table tbl_usuario(
+   id_usuario integer auto_increment primary key,
+   email varchar(60) not null,
+   senha varchar(20) not null,
+   nome_usuario varchar(30) not null
+);
+
+select * from tbl_usuario;
 
 #delete from imagens where id = 9;
