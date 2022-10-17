b05a5b11c5230955c33c4e6c280024cde728ffbe
Antonio Henrique <88168605+Henriquedacruz@users.noreply.github.com>
2022-10-16 23:34:17 -0300

    clear-folder

--- banco/Intec_TS_DDL1_3.sql
+++ banco/Intec_TS_DDL1_3.sql
@@ -3,6 +3,7 @@ create database Intec_TS character set utf8mb4 collate utf8mb4_unicode_ci;
 use Intec_TS;
 
 select * from tbl_user;
+#drop table imagens;
 
 #description "tbl_course_schedule": identificação dos nomes dos períodos de oferecimento de cursos presenciais.
 create table imagens(
