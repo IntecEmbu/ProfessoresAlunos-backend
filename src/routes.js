673800532a9af8b690aa493715523a353195accc
Antonio Henrique <88168605+Henriquedacruz@users.noreply.github.com>
2022-11-24 21:06:32 -0300

    clear

--- src/routes.js
+++ src/routes.js
@@ -4,21 +4,13 @@ import user from "./controllers/userController.js";
 import course from "./controllers/courseController.js";
 // import { verifyJWT } from "./middlewares/jwt.js";
 import login from "./controllers/loginController.js";
-import PostObs from "./controllers/controlerObservatorio.js";
-import ListObs from "./controllers/controlerObservatorio.js";
-import permUser from "./controllers/permUserController.js";
-//import setPermUser from './controllers/setPermUserController.js';
-//import controlerGet from "./controllers/controlerGet.cjs;
 const route = express.Router();
 
-//redirecioan as requisições para a controller correspondente
+//redireciona as requisições para a controller correspondente
 route.use('/user', user);
 route.use('/course', course);
 route.use("/login", login);
-route.use('/PostObs', PostObs);
-route.use('/ListObs', ListObs);
-route.use('/permUser', permUser);
-//route.use('/ListObs', controlerGet);
+
 
 
 
