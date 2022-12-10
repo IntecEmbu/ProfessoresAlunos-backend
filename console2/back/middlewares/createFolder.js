ab15d82e7e2dfd9cbddefe41cb9e51d4177c51c5
Andreicsantana <andrei.santana72@gmail.com>
2022-12-10 00:05:03 -0300

    modal obs

--- console2/back/middlewares/createFolder.js
+++ console2/back/middlewares/createFolder.js
@@ -1,14 +1,30 @@
-const fs = require('fs')
+const fs = require('fs');
+const { connect } = require('http2');
+const { ConnectionAcquireTimeoutError } = require('sequelize');
 
 function createFolder(req, res, next) {
   const {nome} = req.params;
-  if (!fs.existsSync(`/${nome}`)) {
+  try{
+  fs.mkdirSync(`./files/${nome}`)}
+  catch{
+    next();
+  }
 
-    fs.mkdirSync(`./files/${nome}`)
- }
-
-  next();
 }
 
 module.exports = {createFolder};
+
+// const fs = require('fs')
+
+// function createFolder(req, res, next) {
+//   const {nome} = req.params;
+//   if (!fs.existsSync(`/${nome}`)) {
+
+//     fs.mkdirSync(`./files/${nome}`)
+//  }
+
+//   next();
+// }
+
+// module.exports = {createFolder};
 
